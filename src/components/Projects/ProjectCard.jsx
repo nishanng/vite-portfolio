3326bac89ea1696b3335b30481615db0da9df83b
nishan gautam <nishangautam.ng@gmail.com>
2024-09-01 15:02:58 +1000

    updated again

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ProjectCard.css';
 
-const ProjectCard = ({githubRepo, prompt, mainTitle }) => {
+const ProjectCard = ({ githubRepo, prompt, mainTitle }) => {
   const handleCardClick = () => {
     window.open(githubRepo, '_blank', 'noopener noreferrer');
   };
@@ -9,31 +9,9 @@ const ProjectCard = ({githubRepo, prompt, mainTitle }) => {
   return (
     <div className="container noselect" onClick={handleCardClick}>
       <div className="canvas">
-        <div className="tracker tr-1"></div>
-        <div className="tracker tr-2"></div>
-        <div className="tracker tr-3"></div>
-        <div className="tracker tr-4"></div>
-        <div className="tracker tr-5"></div>
-        <div className="tracker tr-6"></div>
-        <div className="tracker tr-7"></div>
-        <div className="tracker tr-8"></div>
-        <div className="tracker tr-9"></div>
-        <div className="tracker tr-10"></div>
-        <div className="tracker tr-11"></div>
-        <div className="tracker tr-12"></div>
-        <div className="tracker tr-13"></div>
-        <div className="tracker tr-14"></div>
-        <div className="tracker tr-15"></div>
-        <div className="tracker tr-16"></div>
-        <div className="tracker tr-17"></div>
-        <div className="tracker tr-18"></div>
-        <div className="tracker tr-19"></div>
-        <div className="tracker tr-20"></div>
-        <div className="tracker tr-21"></div>
-        <div className="tracker tr-22"></div>
-        <div className="tracker tr-23"></div>
-        <div className="tracker tr-24"></div>
-        <div className="tracker tr-25"></div>
+        {[...Array(25)].map((_, i) => (
+          <div key={i} className={`tracker tr-${i + 1}`}></div>
+        ))}
         <div id="card">
           <p id="prompt">{prompt}</p>
           <div className="title">{mainTitle}</div>
