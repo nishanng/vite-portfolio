4ec69b152807e78f8dc6f0395cadf91f5756f9d0
nishan gautam <nishangautam.ng@gmail.com>
2024-09-01 14:59:47 +1000

    made some changes to project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,46 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './ProjectCard.css';
 
-const ProjectCard = ({githubRepo, prompt, mainTitle }) => {
+const TrackerDivs = () => (
+  <>
+    {[...Array(25)].map((_, i) => (
+      <div key={i} className={`tracker tr-${i + 1}`}></div>
+    ))}
+  </>
+);
+
+const ProjectCard = ({ githubRepo, prompt, mainTitle }) => {
   const handleCardClick = () => {
-    window.open(githubRepo, '_blank', 'noopener noreferrer');
+    window.open(githubRepo, '_blank', 'noopener,noreferrer');
   };
 
   return (
-    <div className="container noselect" onClick={handleCardClick}>
+    <article className="container noselect" onClick={handleCardClick}>
       <div className="canvas">
-        <div className="tracker tr-1"></div>
-        <div className="tracker tr-2"></div>
-        <div className="tracker tr-3"></div>
-        <div className="tracker tr-4"></div>
-        <div className="tracker tr-5"></div>
-        <div className="tracker tr-6"></div>
-        <div className="tracker tr-7"></div>
-        <div className="tracker tr-8"></div>
-        <div className="tracker tr-9"></div>
-        <div className="tracker tr-10"></div>
-        <div className="tracker tr-11"></div>
-        <div className="tracker tr-12"></div>
-        <div className="tracker tr-13"></div>
-        <div className="tracker tr-14"></div>
-        <div className="tracker tr-15"></div>
-        <div className="tracker tr-16"></div>
-        <div className="tracker tr-17"></div>
-        <div className="tracker tr-18"></div>
-        <div className="tracker tr-19"></div>
-        <div className="tracker tr-20"></div>
-        <div className="tracker tr-21"></div>
-        <div className="tracker tr-22"></div>
-        <div className="tracker tr-23"></div>
-        <div className="tracker tr-24"></div>
-        <div className="tracker tr-25"></div>
-        <div id="card">
-          <p id="prompt">{prompt}</p>
-          <div className="title">{mainTitle}</div>
+        <TrackerDivs />
+        <div className="card">
+          <p className="prompt">{prompt}</p>
+          <h2 className="title">{mainTitle}</h2>
         </div>
       </div>
-    </div>
+    </article>
   );
+};
+
+ProjectCard.propTypes = {
+  githubRepo: PropTypes.string.isRequired,
+  prompt: PropTypes.string.isRequired,
+  mainTitle: PropTypes.string.isRequired,
 };
 
 export default ProjectCard;
